40a8bf2fe94a1cf06a1a015f5a13d41a9214da8d
Charles E. NZE <charles@charlesen.fr>
2013-01-07 12:42:30 +0100

    MAJ Debug file

--- debug.php
+++ debug.php
@@ -1,6 +1,6 @@
 <?php
 /*
- \* Yanis Framework is Yet ANother Internet Software for web and mobile technologists
+ * Yanis Framework is Yet ANother Internet Software for web and mobile technologists
  * @copyright Copyright (C) 2012, Charles EDOU NZE & Mobile-tuts!
  * @license Released under the MIT License.
  * @author Charles EDOU NZE
