10a5db3508c0de181776135096044ec8f8975b61
Charles E. NZE <charles@charlesen.fr>
2013-01-07 12:22:59 +0100

    Delete of special stuffs

---
+++ system/languages/en/core.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * English
+ */
+return array(
+    'APP_SLOGAN' => 'Share eXperiences, eXchange ideas',
+	'APP_SLOGAN2' => 'The Social and accountable network',
+	'APP_TIPS' => 'Tips',
+	'APP_ALL_TIPS' => 'All Tips',
+	'APP_DEALS' => 'Deals',
+	'APP_FAVORITES' => 'Favorites',
+	'APP_SEARCH' => 'Search',
+	'APP_FIND' => 'Find',
+	'APP_SHARE' => 'Share',
+	'APP_USE' => 'Use',
+	'APP_FIND_TITLE' => 'Find solutions',
+	'APP_SHARE_TITLE' => 'Share your tips',
+	'APP_USE_TITLE' => 'Enjoy good deals',
+	'APP_SHARE_MORE' => 'Share experiences',
+	'APP_SHARE_TIPS' => 'Share tips',
+	'APP_SUGGEST_IDEAS' => 'Suggest ideas',
+
+	'APP_MORE' => 'More',
+	'APP_MORE_IDEAS'=>'More ideas',
+	'APP_MORE_ACTION' => 'More actions',
+	'APP_MORE_DETAILS' => 'More details',
+
+	'APP_TEXT_AND' => 'and',
+	'APP_TEXT_OR' => 'Or',
+	'APP_TEXT_CHANGE_WORLD' => 'change the world',
+
+	'APP_ACCOUNT_LOGIN' => 'Login',
+	'APP_ACCOUNT_LOGOUT' => 'Logout',
+	'APP_ACCOUNT_REGISTERED' => 'Have already an account',
+	'APP_ACCOUNT_NOT_REGISTERED' => 'Not yet registered',
+	'APP_ACCOUNT_REGISTER' => 'Register',
+	'APP_ACCOUNT_NAME' => 'Name',
+	'APP_ACCOUNT_EMAIL' => 'Email adress',
+	'APP_ACCOUNT_PWD' => 'Password',
+	'APP_ACCOUNT_PWD_NOTE' => 'Enter password with at least 6 characters',
+	'APP_ACCOUNT_REMEMBER_ME' => 'Remember me',
+	'APP_ACCOUNT_FORGET_PWD' => 'Password forgotten',
+	'APP_ACCOUNT_OR' => 'Or',
+	'APP_ACCOUNT_CONNECTWITH' => 'Connect with',
+
+	'APP_WELCOME_PERSONALITY' => 'Your personality',
+	'APP_WELCOME_INTEREST' => 'Your interests',
+	'APP_WELCOME_FINISH' => 'Finish',
+	'APP_WELCOME_NEXT' => 'Suivant',
+
+	'APP_MENU_HOME' => 'Home',
+	'APP_MENU_START' => 'Discover',
+	'APP_MENU_PROFIL' => 'Profile',
+	'APP_MENU_BUDGET' => 'Budget',
+	'APP_MENU_SUGGEST' => 'Suggestions',
+	'APP_MENU_MORE' => 'More',
+	'APP_MENU_JOBS' => 'Jobs',
+	'APP_MENU_BLOG' => 'Blog',
+	'APP_MENU_SERVICES' => 'Services',
+	'APP_MENU_PARAMS' => 'Parameters',
+	'APP_MENU_HELP' => 'Help',
+
+	'APP_TIMELINE_WHATSNEW'=>'What\'s new',
+	'APP_TIMELINE_DAYS'=>'Days',
+	'APP_TIMELINE_HOURS'=>'Hours',
+	'APP_TIMELINE_SECONDES'=>'Secondes',
+
+    'APP_COPYRIGHT' => 'All rights reserved',
+
+	'APP_EVENT_LOADING'=>'Loading',
+	'APP_EVENT_ERROR'=>'Error',
+
+	'APP_ACTION_REMOVE'=>'Remove',
+	'APP_ACTION_REMOVE_TEXT'=>'Delete this content',
+	'APP_ACTION_EDIT'=>'Edit',
+	'APP_ACTION_EDIT_TEXT'=>'Edit this content',
+	'APP_ACTION_LOCATION'=>'Location',
+	'APP_ACTION_LOCATION_TEXT'=>'Add a location',
+	'APP_ACTION_ADD'=>'Add',
+	'APP_ACTION_ADD_FAV'=>'Add to favorites',
+	'APP_ACTION_LIKE'=>'Like',
+	'APP_ACTION_SHARE'=>'Share',
+	'APP_ACTION_SHARE_TEXT'=>'Share this content',
+
+	'SYSTEM_DATE_HOUR'=>'hour',
+	'SYSTEM_DATE_MINUTE'=>'minute',
+	'SYSTEM_DATE_SECOND'=>'second',
+	'SYSTEM_DATE_DAY'=>'day',
+	'SYSTEM_DATE_WEEK'=>'week',
+	'SYSTEM_DATE_MONTH'=>'month',
+	'SYSTEM_DATE_YEAR'=>'year',
+	'SYSTEM_DATE_LT_5'=>'less than 5 seconds',
+	
+	'APP_SYSTEM_SQL_NORESULT'=>'Oops ! There is no result'
+);
+?>
